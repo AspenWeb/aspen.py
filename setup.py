4e49c010b313da07d48c9dbbf1879fbb19218e42
Changaco <changaco@changaco.oy.lc>
2019-11-16 11:27:49 +0100

    release version 1.0

--- setup.py
+++ setup.py
@@ -1,42 +1,32 @@
 from os.path import dirname, join
 
-try:
-    import setuptools  # noqa
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-
 from setuptools import find_packages, setup
 
-version = open('version.txt').read()
+root = dirname(__file__)
 
-
-classifiers = [ 'Development Status :: 4 - Beta'
-              , 'Environment :: Console'
-              , 'Intended Audience :: Developers'
-              , 'License :: OSI Approved :: MIT License'
-              , 'Natural Language :: English'
-              , 'Operating System :: OS Independent'
-              , 'Programming Language :: Python :: 2.7'
-              , 'Programming Language :: Python :: 3.4'
-              , 'Programming Language :: Python :: 3.5'
-              , 'Programming Language :: Python :: 3.6'
-              , 'Programming Language :: Python :: 3.7'
-              , 'Programming Language :: Python :: Implementation :: CPython'
-              , 'Topic :: Internet :: WWW/HTTP :: WSGI :: Application'
-               ]
-
-setup( author = 'Chad Whitacre et al.'
-     , author_email = 'team@aspen.io'
-     , classifiers = classifiers
-     , description = 'A filesystem router for Python web frameworks'
-     , long_description=open(join(dirname(__file__), 'README.rst')).read()
-     , name = 'aspen'
-     , packages = find_packages()
-     , url = 'https://github.com/AspenWeb/aspen.py'
-     , version = version
-     , zip_safe = False
-     , package_data = {'aspen': ['request_processor/mime.types']}
-     , install_requires = open('requirements.txt').read()
-     , tests_require = open('requirements_tests.txt').read()
-      )
+setup(
+    author = 'Chad Whitacre et al.',
+    author_email = 'team@aspen.io',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description = 'A filesystem router for Python web frameworks',
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    long_description_content_type='text/x-rst',
+    name = 'aspen',
+    url = 'https://github.com/AspenWeb/aspen.py',
+    version = open(join(root, 'version.txt')).read().strip(),
+    zip_safe = False,
+    packages = find_packages(),
+    package_data = {'aspen': ['request_processor/mime.types']},
+    install_requires = open(join(root, 'requirements.txt')).read(),
+    tests_require = open(join(root, 'requirements_tests.txt')).read(),
+)
